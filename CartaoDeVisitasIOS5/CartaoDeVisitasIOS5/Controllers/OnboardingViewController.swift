29f42557ac2b00383f3bdb2448cc5ca81083a685
Ana Carolina Barbosa de Souza <souzaanacarolb@gmail.com>
2024-06-20 09:43:18 -0300

    feat: update main

---
+++ CartaoDeVisitasIOS5/CartaoDeVisitasIOS5/Controllers/OnboardingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingViewController.swift
+//  CartaoDeVisitasIOS5
+//
+//  Created by Ana Carolina Barbosa de Souza on 18/06/24.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
